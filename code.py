5f2bb14cac7ba39e2891357d96989d10666f0359
HADI KALEEM <hadikaleem@192.168.10.186>
2025-10-19 18:52:46 +0500

    Added farewell function in feature branch

--- code.py
+++ code.py
@@ -1,5 +1,9 @@
 def greet():
     print("Hello from my new demo Git project!")
 
+def farewell():
+    print("Goodbye from the feature branch!")
+
 if __name__ == "__main__":
     greet()
+    farewell()
